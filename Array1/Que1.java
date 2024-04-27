8a1228ac8e5a41902d99668a9b6afb9b2151d87e
Shraddhavalyal <145037969+Shraddhavalyal@users.noreply.github.com>
2024-04-27 16:31:35 +0530

    Create Que1.java

---
+++ Array1/Que1.java
@@ -0,0 +1,15 @@
+class ARRAYDEMO{
+        public static void main(String []args){
+                int arr[]=new int[]{10,20,30,40,50,60,70,80,90,100};
+                System.out.print(arr[0]+",");
+                System.out.print(arr[1]+",");
+                System.out.print(arr[2]+",");
+                System.out.print(arr[3]+",");
+                System.out.print(arr[4]+",");
+                System.out.print(arr[5]+",");
+                System.out.print(arr[6]+",");
+                System.out.print(arr[7]+",");
+                System.out.print(arr[8]+",");
+                System.out.print(arr[9]+",");
+        }
+}
